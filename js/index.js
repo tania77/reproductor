c9b026c9612515b217745a93d6dd6b74d274d4b8
Tania González Rodríguez <ainatrg@gmail.com>
2016-10-25 18:33:16 +0100

    reproductor

---
+++ js/index.js
@@ -0,0 +1,36 @@
+var songs = ["http://scummbar.com/mi2/MI1-CD/01%20-%20Opening%20Themes%20-%20Introduction.mp3", "http://scummbar.com/mi2/MI1-CD/02%20-%20Chapter%20Screen.mp3", "http://scummbar.com/mi2/MI1-CD/03%20-%20The%20Scumm%20Bar.mp3"];
+var currentSong = 0;
+
+var audio= document.getElementById('song');
+var lcd = document.getElementById('lcd');
+
+function mostrar() {
+  lcd.innerHTML = "Cancion";
+}
+
+function reproducir() {
+  mostrar();
+  audio.src = songs[currentSong];
+  audio.play();
+}
+function pausa() {
+  audio.pause();
+}
+function siguiente() {
+  if (currentSong == (songs.length-1)){
+    currentSong = 0;
+  }
+  else {
+    currentSong++;
+  }
+  reproducir();
+}
+function anterior() {
+  if (currentSong == 0){
+    currentSong = songs.length-1;
+  }
+  else {
+    currentSong--;
+  }
+  reproducir();
+}
